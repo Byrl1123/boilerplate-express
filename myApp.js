31b02c12cd1243544640370b1f0bd535fc1bd36d
Bryant <bryantrevete189@gmail.com>
2023-09-27 00:58:16 -0400

    committing app.listen() method

--- myApp.js
+++ myApp.js
@@ -4,10 +4,16 @@ let app = express();
 let message = "Hello World";
 console.log(message);
 
+app.get('/server.js', (req, res) => {
 
+    res.send("Hello Express");
 
+});
 
-
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("app running in port 3000!");
+})
 
 
 
